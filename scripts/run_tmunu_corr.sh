42f7f550e4cc24f4ec0356f412570df2a148b941
willian-m <not.public@not.public.com>
2018-01-26 22:26:19 +0000

    Script to run the tmunu_corr.run

---
+++ scripts/run_tmunu_corr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#This script assumes it is being run from inside the project root directory
+DIR=/home/willian/git_repo/SU2SimSuit
+
+Ns=8
+Nt=8
+BETA=2.8
+filePath=$DIR/output/links008008008008beta2.80Sweep
+COUNTER=1000
+ENDCOUNTER=10000
+STEP=10
+
+#--------------------------------------------
+
+while [  $COUNTER -le $ENDCOUNTER ]; do #For each configuration
+   echo "Processing lattice no. "$COUNTER
+   COUNTERf=`printf '%04d' $COUNTER`
+   
+   cd $DIR/output
+   $DIR/bin/tmunu_corr.run $Ns $Ns $Ns $Nt $BETA $filePath$COUNTERf".dat"
+
+   if [ $? -eq "1" ]; then
+      echo  "Something went terribly wrong!!!"
+      break
+   fi
+   let COUNTER=COUNTER+STEP
+done
+
