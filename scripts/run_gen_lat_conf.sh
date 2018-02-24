c5c93789456c511f8d3ef8cf3368951baec980c4
willian-m <willian.matioli@gmail.com>
2018-02-24 03:28:33 +0000

    Streamlined scripts to keep organize in folders the output of each program.

--- scripts/run_gen_lat_conf.sh
+++ scripts/run_gen_lat_conf.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-#This script assumes it is being run from inside the project root directory
+#This script uses the following folder as root directory of the project
 DIR=/home/willian/git_repos/SU2SimSuit
-
+DIR=`pwd`
 Ns=8
 Nt=8
 BETA=2.8
-filePath=$DIR/output/links008008008008beta2.80Sweep
+#filePath=$DIR/output/links008008008008beta2.80Sweep
 NMC=10000
 STEP=10
 
 #--------------------------------------------
-cd $DIR/output
+mkdir -p $DIR/output
+mkdir -p $DIR/output/lat_conf
+cd $DIR/output/lat_conf
 $DIR/bin/gen_lat_conf.run $Ns $Ns $Ns $Nt $BETA H $NMC $STEP 
 cd $DIR
--- scripts/run_tmunu_corr.sh
+++ scripts/run_tmunu_corr.sh
@@ -2,11 +2,12 @@
 
 #This script assumes it is being run from inside the project root directory
 DIR=/home/willian/git_repos/SU2SimSuit
-
+DIR=`pwd`
+#--------------------------------------------
 Ns=8
 Nt=8
 BETA=2.8
-filePath=$DIR/output/links008008008008beta2.80Sweep
+filePath=$DIR/output/lat_conf/links008008008008beta2.80Sweep
 COUNTER=1000
 ENDCOUNTER=10000
 STEP=10
@@ -25,4 +26,8 @@ while [  $COUNTER -le $ENDCOUNTER ]; do #For each configuration
    fi
    let COUNTER=COUNTER+STEP
 done
+#Program output files on the same folder as input files.
+#We fix this here
+mkdir -p $DIR/output/T0iT0j
+mv $DIR/output/lat_conf/*source*.dat $DIR/output/T0iT0j/
 cd $DIR
