2fdbdfb2e3a18fd795de1fc68eed686da79ca1d7
willian-m <not.public@not.public.com>
2018-02-26 20:12:24 +0000

    Fixes bugs on argument reading and on file loading, as well as add progress messages.

--- src/statisticalAverager.f90
+++ src/statisticalAverager.f90
@@ -19,16 +19,20 @@ call readParameters
 numConf = (NMCEnd-NMCStart)/step
 
 
+print*, "Allocating arrays..."
 allocate( average(nx*ny*nz*nt) )
+allocate( rawData(nx*ny*nz*nt) )
+allocate( errors(nx*ny*nz*nt) )
 allocate( binnedData(nx*ny*nz*nt,Nbins))
 
 average = dcmplx(0.d0,0.d0)
 
+print *, "Performing statistical average..."
 do NMC=NMCStart,NMCEnd,step !Average over MC configurations
    !Loads the data
    inquire(iolength=reclen) rawData(1)
    open(unit=NMC,access='direct',form='unformatted',recl=reclen)
-   do x=1,nx*ny*nz*nt+1 !In each site
+   do x=1,nx*ny*nz*nt !In each site
       read(unit=NMC,rec=x) rawData(x) 
    end do
    close(NMC)
@@ -64,6 +68,7 @@ end do
 
 errors = errors*(Nbins-1)/Nbins
 
+print *, "Saving files..."
 open(unit=1,file="StatisticalAverage.dat",form="unformatted",access="direct",recl=reclen)
 open(unit=2,file="StatisticalError.dat",form="unformatted",access="direct",recl=reclen)
 do x=1,nx*ny*nz*nt
@@ -72,6 +77,12 @@ do x=1,nx*ny*nz*nt
 end do
 close(1)
 close(2)
+
+deallocate(average, rawData, errors, binnedData)
+
+
+print*, "Files saved and vectors deallocated."
+print*, "All jobs done. See you. o/"
 
 contains
 
@@ -96,6 +107,7 @@ contains
          call GET_COMMAND_ARGUMENT(5,argNMCStartStep)
          call GET_COMMAND_ARGUMENT(6,argNMCEndStep)
          call GET_COMMAND_ARGUMENT(7,argNMCStepSize)
+         call GET_COMMAND_ARGUMENT(8,argNbins)
          read(argNx,*) nx
          read(argNy,*) ny
          read(argNz,*) nz
@@ -103,6 +115,7 @@ contains
          read(argNMCStartStep,*) NMCStart
          read(argNMCEndStep,*) NMCEnd
          read(argNMCStepSize,*) step
+         read(argNbins,*) Nbins
       end if
    end subroutine
 
