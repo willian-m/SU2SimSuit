b49beb557378fd54b1380195af2e2a5b113f96e6
willian-m <willian.serenone@usp.br>
2018-02-27 17:14:45 -0300

    Changed binary writting and reading to file from sequential to direct access.

--- src/modules/IOfunctions.f90
+++ src/modules/IOfunctions.f90
@@ -18,22 +18,35 @@ module IOfunctions
 
    use lattice
    implicit none
-   
+   integer :: record_len
    contains
       
       subroutine writeLattice(sweepNum)
          integer,intent(in) :: sweepNum
+         integer*8 :: i,dir
          character(len=50) :: fileName
+         inquire(iolength=record_len) U(1,1)%a
          write(filename,"('links',4I3.3,'beta',F4.2,'Sweep',I9.9,'.dat')") nx,ny,nz,nt,beta,sweepNum
-         open(unit=1,status='replace',file=filename,form='unformatted')
-         write(1) U 
+         open(unit=1,status='replace',file=filename,form='unformatted',access='direct',recl=record_len)
+         do i=0,nx*ny*nz*nt-1
+            do dir=1,8
+               write(1,rec=dir+8*i) U(dir,i)%a
+            end do
+         end do
          close(1)
       end subroutine
 
       subroutine readLattice(filename)
          character(len=1024), intent(in) :: filename
-         open(unit=10,status='old',file=filename,form='unformatted')
-         read(10) U
+         integer*8 :: i,dir
+
+         inquire(iolength=record_len) U(1,1)%a
+         open(unit=10,status='old',file=filename,form='unformatted',access='direct',recl=record_len)
+         do i=0,nx*ny*nz*nt-1
+            do dir=1,8
+               read(10,rec=dir+8*i) U(dir,i)%a
+            end do
+         end do
          close(10)
       end subroutine readLattice
 
